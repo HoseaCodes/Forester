6ac4b4138787368f10ccf80cc3e44a858c7cea05
Dominique Hosea <dominique11h@yahoo.com>
2024-06-29 00:58:46 -0500

    fix(*): update logs

--- controllers/event.js
+++ controllers/event.js
@@ -1,34 +1,36 @@
-const Logger = require('../logger.js')
-const logger = new Logger('app');
-const os = require('os');
-
-const eventCtrl = {
-	getLogs,
-};
+/* eslint-disable @typescript-eslint/no-var-requires */
+const Logger = require("../logger.js");
+const logger = new Logger("app");
+const os = require("os");
 
 async function getLogs(req, res, next) {
-    if(req.body === null || undefined) next();
-    
-    try {
-            console.log(req.body)
-            const {app} = req.body
-            console.log(req.url)
-            console.log(os.hostname())
-            console.log(req.originalUrl)
-            const fullUrl = `${req.protocol}://${req.get('host')}${req.url}`;
+  if (req.body === null || undefined) next();
+  try {
+    console.log(req.body);
+    const { app, message } = req.body;
+    console.log(req.url);
+    console.log(os.hostname());
+    console.log(req.originalUrl);
+    const fullUrl = `${req.protocol}://${req.get("host")}${req.url}`;
 
-            let log = new Logger(`${app}`);
-            if(log) {
-                log.info(`Forester log captured url: ${fullUrl} | callingApp: ${app} `);
-            }
-            logger.info(`Forester log captured url: ${fullUrl} | callingApp: ${app} `);   
-            res.json({ status: "Success"})
-        } catch (err) {
-            logger.error(err);
-            
-            return res.status(500).json({ msg: err.message })
-        }
+    const log = new Logger(`${app}`);
+    if (log) {
+      log.info(
+        `From Forester log captured: url: ${fullUrl} | callingApp: ${app} | hostname: ${os.hostname()} | msg: ${message}`
+      );
+    }
+    logger.info(
+      `Forester log captured url: ${fullUrl} | callingApp: ${app} | hostname: ${os.hostname()} | msg: ${message} `
+    );
+    return res.json({ status: "Success" });
+  } catch (err) {
+    logger.error(err);
+    return res.status(500).json({ msg: err.message });
+  }
 }
 
+const eventCtrl = {
+  getLogs,
+};
 
 module.exports = eventCtrl;
